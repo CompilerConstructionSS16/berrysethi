f22a36da4e2485c08c6692c6996ee33af373d369
JeGa <jega@win>
2016-07-17 18:22:07 +0200

    Minor changes.

--- src/projects/NFAGeneratorBerrySethi/TransitionTableVisitor.java
+++ src/projects/NFAGeneratorBerrySethi/TransitionTableVisitor.java
@@ -2,21 +2,41 @@ package projects.NFAGeneratorBerrySethi;
 
 import nfa.TransitionTable;
 import regex.Char;
+import regex.RegularExpression;
 import regex.RegularExpressionVisitor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TransitionTableVisitor implements RegularExpressionVisitor {
 
-    // Needs access to attributes ...
+    ArrayList<Char> chars = new ArrayList<>();
 
     @Override
     public void visit(Char character) {
-
+        chars.add(character);
     }
 
-    public TransitionTable getTable() {
+    public TransitionTable getTable(
+            RegularExpression root,
+            HashMap<RegularExpression, Boolean> empty,
+            HashMap<RegularExpression, ArrayList<RegularExpression>> first,
+            HashMap<RegularExpression, ArrayList<RegularExpression>> next,
+            HashMap<RegularExpression, ArrayList<RegularExpression>> last) {
+
         TransitionTable table;
 
+        // Add new root state.
         // TODO
+
+        if (empty.get(root))
+            // TODO
+
+        for (RegularExpression exp : last.get(root))
+            // TODO
+
+
+
 
         return table;
     }
