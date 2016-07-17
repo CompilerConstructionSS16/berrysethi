451c48850299e6c4cfa27683247de6b16eec77c0
JeGa <jega@win>
2016-07-17 22:36:41 +0200

    Formatting.

--- src/regex/RegularExpressionVisitor.java
+++ src/regex/RegularExpressionVisitor.java
@@ -30,49 +30,39 @@ public interface RegularExpressionVisitor {
         post((UnaryExpression) expression);
     }
 
-
     default void pre(OptionalExpression expression) {
         pre((UnaryExpression) expression);
     }
-
 
     default void post(KleeneStarExpression expression) {
         post((UnaryExpression) expression);
     }
 
-
     default void pre(KleeneStarExpression expression) {
         pre((UnaryExpression) expression);
     }
-
 
     default void post(PlusExpression expression) {
         post((UnaryExpression) expression);
     }
 
-
     default void pre(PlusExpression expression) {
         pre((UnaryExpression) expression);
     }
-
 
     default void post(UnaryExpression expression) {
         post((RegularExpression) expression);
     }
 
-
     default void pre(UnaryExpression expression) {
         pre((RegularExpression) expression);
     }
 
-
     default void visit(Char character) {
     }
 
-
     default void pre(RegularExpression expression) {
     }
-
 
     default void post(RegularExpression expression) {
     }
